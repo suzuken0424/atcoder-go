e43cef6b69ad14154e39dd965e9ed687daebf546
suzuken0424 <s.kenta0424@gmail.com>
2023-11-14 15:23:48 +0900

    templateに標準入力・標準出力・stringToIntSlice関数・flush関数を追加

--- template.go
+++ template.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"math"
 	"os"
+	"strconv"
 )
+
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+const baseRune = 'a'
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	defer flush()
+}
 
 // スライスの重複削除
 func sliceUniq[T comparable](arr []T) []T {
@@ -31,7 +48,61 @@ func checkMatchingDice(num int) bool {
 	return true
 }
 
-var scanner = bufio.NewScanner(os.Stdin)
+/*
+バッファにたまったデータを出力先に書き込む際にエラーが発生した場合に、
+そのエラーを検知し、プログラムを停止させる役割を果たします。これにより、
+データの書き込みが確実に行われるようになります。
+*/
+func flush() {
+	e := wtr.Flush()
+	if e != nil {
+		panic(e)
+	}
+}
 
-func main() {
+// 標準入力の1つの数字を取得する
+func inputInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// 標準入力の2つの数字を取得する
+func input2Int() (int, int) {
+	return inputInt(), inputInt()
+}
+
+// 標準入力の文字列を取得する
+func inputString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// 文字列をintのsliceに変換する
+func stringToIntSlice(s string) []int {
+	r := make([]int, len(s))
+	for i, v := range s {
+		r[i] = int(v - baseRune)
+	}
+	return r
+}
+
+// intのsliceを作成する
+func createIntSlice(length int, defaultNum int) []int {
+	sl := make([]int, length)
+	for i := 0; i < length; i++ {
+		sl[i] = defaultNum
+	}
+	return sl
+}
+
+// 結果を出力
+func outputResult(v ...interface{}) {
+	_, e := fmt.Fprintln(wtr, v...)
+	if e != nil {
+		panic(e)
+	}
 }
