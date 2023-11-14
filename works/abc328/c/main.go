0dc47cff8ab0ebdffab937db23a020ab49aeafd5
suzuken0424 <s.kenta0424@gmail.com>
2023-11-14 15:24:22 +0900

    abc328のc問題を解く

--- works/abc328/c/main.go
+++ works/abc328/c/main.go
@@ -3,30 +3,98 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
 )
 
-var scanner = bufio.NewScanner(os.Stdin)
+var sc = bufio.NewScanner(os.Stdin)
+var wtr = bufio.NewWriter(os.Stdout)
+
+const baseRune = 'a'
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+}
 
 func main() {
-	var n int
-	var q int
-	var s string
+	defer flush()
 
-	fmt.Scanf("%d %d", &n, &q)
-	fmt.Scan(&s)
+	n, q := input2Int()
+	s := stringToIntSlice(inputString())
+	count := createIntSlice(n, 0)
 
-	for i := 0; i < q; i++ {
-		var l int
-		var r int
-		fmt.Scanf("%d %d", &l, &r)
-
-		var count int
-		for j := l - 1; j < r-1; j++ {
-			if s[j] == s[j+1] {
-				count++
-			}
+	// 累積和の前準備
+	for i := 1; i < n; i++ {
+		if s[i-1] == s[i] {
+			count[i] = count[i-1] + 1
+		} else {
+			count[i] = count[i-1]
 		}
-		fmt.Println(count)
+	}
+	// 累積和の計算
+	for i := 0; i < q; i++ {
+		l, r := input2Int()
+		outputResult(count[r-1] - count[l-1])
+	}
+}
+
+/*
+バッファにたまったデータを出力先に書き込む際にエラーが発生した場合に、
+そのエラーを検知し、プログラムを停止させる役割を果たします。これにより、
+データの書き込みが確実に行われるようになります。
+*/
+func flush() {
+	e := wtr.Flush()
+	if e != nil {
+		panic(e)
+	}
+}
+
+// 標準入力の1つの数字を取得する
+func inputInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// 標準入力の2つの数字を取得する
+func input2Int() (int, int) {
+	return inputInt(), inputInt()
+}
+
+// 標準入力の文字列を取得する
+func inputString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// 文字列をintのsliceに変換する
+func stringToIntSlice(s string) []int {
+	r := make([]int, len(s))
+	for i, v := range s {
+		r[i] = int(v - baseRune)
+	}
+	return r
+}
+
+// intのsliceを作成する
+func createIntSlice(length int, defaultNum int) []int {
+	sl := make([]int, length)
+	for i := 0; i < length; i++ {
+		sl[i] = defaultNum
+	}
+	return sl
+}
+
+// 結果を出力
+func outputResult(v ...interface{}) {
+	_, e := fmt.Fprintln(wtr, v...)
+	if e != nil {
+		panic(e)
 	}
 }
